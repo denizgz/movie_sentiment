29d830b194841b4bdd556b9bb0b449c92e72d518
nidsniks <nitishacharaya@yahoo.com>
2021-07-13 20:28:13 +0200

    final

--- sentimentanalysis_streamlit.py
+++ sentimentanalysis_streamlit.py
@@ -13,41 +13,38 @@ import preprocessor as p
 from textblob import TextBlob
 import streamlit as st
 from wordcloud import WordCloud, STOPWORDS
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 # In[43]:
 
-
+# connect to hiv
 conn = hive.connect('seneca1.lehre.hwr-berlin.de', port=10000, username='nitish_acharya', database= 'default', password='BIPMinitial', auth='LDAP')
 
 
 # In[45]:
 
-
+# function for adjective
 def get_adjectives(text):
     blob = TextBlob(text)
     return [ word for (word,tag) in blob.tags if tag == "JJ"]
 
-
+analyzer = SentimentIntensityAnalyzer()
 # In[46]:
 
-
+#import the data and use vader sentiment analysis
 df = pd.read_sql("SELECT * FROM movierater", conn)
 df1 = df[['movierater.tweet', 'movierater.keyword_new','movierater.retweets','movierater.location']]
 df1['prep']=[p.clean(doc) for doc in df1['movierater.tweet']]
-polarity = lambda x: TextBlob(x).sentiment.polarity
-df1['sentiment_tb'] = df1['prep'].apply(polarity)
-df1['sentiment_tb_desc']=df1['sentiment_tb'].apply(lambda x: 'positive' if x > 0  else 'negative' if x < -0 else 'neutral')
-polarity = lambda x: TextBlob(x).sentiment.polarity
-df1['sentiment_tb'] = df1['prep'].apply(polarity)
-df2 = pd.concat([df1,pd.get_dummies(df1['sentiment_tb_desc'], prefix='rating')],axis=1)
-df2 = df2.groupby('movierater.keyword_new')['rating_neutral','rating_positive','rating_negative'].sum()
-df2
+df1['movierater.location']=[p.clean(doc) for doc in df1['movierater.location']]
+df1['sentiment_tb'] = df1['prep'].apply(analyzer.polarity_scores)
+df1 = pd.concat([df1.drop(['sentiment_tb'], axis=1), df1['sentiment_tb'].apply(pd.Series)], axis=1)
+df1
 
 
 # In[47]:
 
-
+df1['sentiment_tb_desc']=df1['compound'].apply(lambda x: 'positive' if x >= 0.05  else 'negative' if x < -0.05 else 'neutral')
 df1
 
 
@@ -60,6 +57,8 @@ df1.to_csv(r'C:/Users/nitis/OneDrive/Dokumente/movie_sentiment/streamlit_app/oth
 # In[49]:
 
 
+df2 = pd.concat([df1,pd.get_dummies(df1['sentiment_tb_desc'], prefix='rating')],axis=1)
+df2 = df2.groupby('movierater.keyword_new')['rating_neutral','rating_positive','rating_negative'].sum()
 df2
 
 
